1c7b27785854670599b2b15921a6e602cbff531a
Michał Kostrzewski <skyflighter.kos@protonmail.com>
2021-10-24 14:48:35 +0200

    test

--- src/main.c
+++ src/main.c
@@ -50,9 +50,6 @@ int program_setup()
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glVertexAttribPointer(vertexPositionAttribute, 3,  GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 
 	return 0;
 }
