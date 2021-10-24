4518443d5c6e1869ac50dd83c7e066e8de4c7e9f
Michał Kostrzewski <skyflighter.kos@protonmail.com>
2021-10-24 14:26:07 +0200

    Add vao and vbo

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ unsigned int indices[] = {
     1, 2, 3    // second triangle
 };
 
-unsigned int ebo;
+unsigned int ebo, vbo, vao;
 static shader_t *shader;
 
 int program_setup()
@@ -33,14 +33,19 @@ int program_setup()
 		return -1;
 	}
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices);
-	glEnableVertexAttribArray(0);
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &ebo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+	glEnableVertexAttribArray(0);
 
 	return 0;
 }
@@ -48,6 +53,7 @@ int program_setup()
 void program_update()
 {
 	glUseProgram(shader->id);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBindVertexArray(vao);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
