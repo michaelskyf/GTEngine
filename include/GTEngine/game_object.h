e77eef9c170973f8050666331ce003549e9629ac
Michał Kostrzewski <skyflighter.kos@protonmail.com>
2021-10-24 22:26:19 +0200

    Prepare for objects

---
+++ include/GTEngine/game_object.h
@@ -0,0 +1,33 @@
+/*
+    This file is part of GTEngine.
+
+    GTEngine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GTEngine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GTEngine. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef GTE_GAME_OBJECT_H
+#define GTE_GAME_OBJECT_H
+
+typedef struct {
+	float position[3];
+	float size[3];
+	float velocity[3];
+	float rotation[3];
+	_Bool destroyed;
+} game_object_t;
+
+game_object_t *game_object_create(float *pos, float *size,
+				  float *vel, float *rot, _Bool destroyed);
+void game_object_destroy(game_object_t *);
+
+#endif
---
+++ include/GTEngine/mesh.h
@@ -0,0 +1,42 @@
+/*
+    This file is part of GTEngine.
+
+    GTEngine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GTEngine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GTEngine. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef GTE_MESH_H
+#define GTE_MESH_H
+
+#include "vector.h"
+
+typedef struct{
+	float position[3];
+	float normal[3];
+	float texture_coords[3];
+}vertex_t;
+
+typedef struct{
+	unsigned int id;
+}texture_t;
+
+typedef struct {
+	Vector *vertices;
+	Vector *indices;
+	Vector *textures;
+}mesh_t;
+
+mesh_t *mesh_create(void);
+void mesh_destroy(mesh_t *);
+
+#endif
