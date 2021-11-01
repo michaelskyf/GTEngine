4c4827368b372c05ee0ad6d00b94f1edab026fd2
michaelskyf <>
2021-11-01 20:31:00 +0100

    Update math.h

--- include/GTEngine/math.h
+++ include/GTEngine/math.h
@@ -1,5 +1,35 @@
+/*
+    This file is part of GTEngine.
+
+    GTEngine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GTEngine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GTEngine. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef GTE_MATH_H
 #define GTE_MATH_H
+
+/*
+ * This file defines:
+ * vec2, vec3, vec4
+ *
+ */
+
+typedef struct {
+	union {
+		struct {float x,y;};
+		float v[2];
+	};
+} vec2;
 
 typedef struct {
 	union {
@@ -7,5 +37,12 @@ typedef struct {
 		float v[3];
 	};
 } vec3;
+
+typedef struct {
+	union {
+		struct {float x,y,z,w;};
+		float v[4];
+	};
+} vec4;
 
 #endif
